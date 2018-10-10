2ec559d118241058f218f311d40ec61bd13f635d
Victor Utreras J <vutreras@live.com>
2018-10-10 11:16:28 -0300

    Update script for config and package the plugin

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 #Script for configure the plugin project
-
-PHP_SDK_VERSION='1.3.2'
+PHP_SDK_VERSION="1.3.2"
 REPO_SDK="https://github.com/TransbankDevelopers/transbank-sdk-php/archive/$PHP_SDK_VERSION.zip"
-DIR_DEST_SDK='src/library/transbank-sdk-php'
+DIR_LIBS="src/transbank_onepay/library"
+DIR_NAME_SDK="transbank-sdk-php"
+DIR_DEST_SDK="$DIR_LIBS/$DIR_NAME_SDK"
 
-echo "Removing the older SDK"
-rm -rf "$DIR_DEST_SDK**"
+echo "Removing the older SDK $DIR_DEST_SDK"
+rm -rf $DIR_DEST_SDK**
 
-echo "Downloading SDK version: $PHP_SDK_VERSION"
-wget $REPO_SDK -O "$PHP_SDK_VERSION.zip"
-unzip "$PHP_SDK_VERSION.zip" -d src/library
+echo "Downloading SDK version: $PHP_SDK_VERSION from: $REPO_SDK"
+curl -O -L $REPO_SDK
+unzip "$PHP_SDK_VERSION.zip" -d $DIR_LIBS
 rm -rf "$PHP_SDK_VERSION.zip"
-mv "$DIR_DEST_SDK-$PHP_SDK_VERSION" $DIR_DEST_SDK
+mv "$DIR_DEST_SDK-$PHP_SDK_VERSION" "$DIR_DEST_SDK"
 
-echo "Changing to SDK version: $PHP_SDK_VERSION"
 echo "SDK version: $PHP_SDK_VERSION"
